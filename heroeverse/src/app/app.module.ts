28be5d6c9866b234b941ab71f724f3be42e059f7
salong01 <salong01@estudiantes.unileon.es>
2020-12-12 23:03:52 +0100

    Login page

--- heroeverse/src/app/app.module.ts
+++ heroeverse/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {path: 'hero-details', component: HeroDetailComponent},
+      {path: 'login', component: LoginComponent}
+    ]),
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ heroeverse/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Strange', description: 'Hechicero supremo y guardián de la gema del tiempo', faction: 'Avenger' },
+  { id: 12, name: 'Namor', description: 'Hijo de un mutante y una reina atlante, actual rey de Atlantis', faction: 'Atlantis' },
+  { id: 13, name: 'Iron Man', description: 'Porpietario de industrias Stark y actual miembro de ', faction: 'Avenger' },
+  { id: 14, name: 'Capitana Marvel', description: 'Expiloto humana criada como kree con poderes concedidos por el cubo cósmico', faction: 'Kree' },
+  { id: 15, name: 'Magneto', description: 'Líder mutante de la hermandad, opositores a la raza humana', faction: 'Brotherhood' },
+  { id: 16, name: 'Wasp', description: 'Compañera de Hanq Pym, creadores de las partículas Pym capaces de hacerte cambiar de tamaño', faction: 'Pym Tecnologies' },
+  { id: 17, name: 'Capitán América', description: 'Supero soldado creado con un suero para ganar la segunda guerra mundial y líder de los vengadores', faction: 'Avenger' },
+  { id: 18, name: 'Mr. Fantatic', description: 'Líder de los cuatro fantásticos con el poder de estirar su cuerpo sin límites', faction: 'Fantastic Four' },
+  { id: 19, name: 'Pyro', description: 'Antiguo miembro de x-men, actual miembro de la Hermandad, muy fiel a Magneto', faction: 'Brotherhood' },
+  { id: 20, name: 'Thanos', description: 'El líder de la orden negra y titán loco, capaz de sostener las gemas del infinito', faction: 'Black Order' }
+];
