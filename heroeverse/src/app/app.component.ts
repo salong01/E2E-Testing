aa8121c3d064c06b03fae2b267d548708d976feb
salong01 <salong01@estudiantes.unileon.es>
2020-12-26 13:51:08 +0100

    Login and register form

--- heroeverse/src/app/app.component.ts
+++ heroeverse/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'heroe verse';
+  title = 'Heroe Verse';
 }
--- heroeverse/src/app/app.module.ts
+++ heroeverse/src/app/app.module.ts
@@ -10,24 +10,35 @@ import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-    { path: 'hero-details', component: HeroDetailComponent },
-    { path: 'login', component: LoginComponent }
-], { relativeLinkResolution: 'legacy' }),
+      { path: 'hero-details', component: HeroDetailComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+    ],
+    { relativeLinkResolution: 'legacy' }),
     FormsModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
