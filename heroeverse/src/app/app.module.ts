575df347d0e1e1ab16a40292ad80b02d45f902c4
salong01 <salong01@estudiantes.unileon.es>
2021-04-19 23:50:49 +0200

    Changed route module file

--- heroeverse/src/app/app.module.ts
+++ heroeverse/src/app/app.module.ts
@@ -3,13 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Validators } from '@angular/forms';
-//import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
-import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
@@ -28,15 +27,9 @@ import { MatRadioModule } from '@angular/material/radio';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    RouterModule.forRoot([
-      { path: 'heroes/:name', component: HeroDetailComponent },
-      { path: 'heroes', component: HeroesComponent },
-      { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegisterComponent },
-      { path: '', redirectTo: 'login', pathMatch: 'full' },
-    ], { relativeLinkResolution: 'legacy', initialNavigation: 'enabled' }),
     FormsModule,
     ReactiveFormsModule,
+    AppRoutingModule,
     //Validators,
     BrowserAnimationsModule,
     MatToolbarModule,
